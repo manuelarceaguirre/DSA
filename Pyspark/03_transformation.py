0e2da719a4bc1fb66f7794bd08bae45a2e2f4eea
Manuel Arce <manuel.arceaguirre@gmail.com>
2025-05-16 16:09:55 -0700

    rep 03

--- Pyspark/03_transformation.py
+++ Pyspark/03_transformation.py
@@ -1,13 +1,14 @@
 """
+transactions
 | Column          | Type    | Example Value         |
 | --------------- | ------- | --------------------- |
-| transaction\_id | string  | "T001"                |
-| customer\_id    | string  | "C123"                |
-| purchase\_date  | string  | "2025-05-01 09:15:00" |
+| transaction_id | string  | "T001"                |
+| customer_id    | string  | "C123"                |
+| purchase_date  | string  | "2025-05-01 09:15:00" |
 | item            | string  | "  Socks   "          |
 | quantity        | integer | 2                     |
 | price           | double  | 9.99                  |
-| coupon\_code    | string  | null / " SPRING2025"  |
+| coupon_code    | string  | null / " SPRING2025"  |
 
 Data Cleansing:
 Trim whitespace from item and coupon_code columns.
@@ -17,3 +18,24 @@ Data Enrichment:
 Create a new column total_amount as quantity * price.
 Create a new column purchase_date_ts as a proper timestamp type, converted from the string purchase_date.
 """
+
+
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.getOrCreate()
+from pyspark.sql import functions as F, timestamp
+
+path = ""
+transactions_df = spark.read.format("delta").load(path)
+transactions_df = transactions_df.withColumn("item", F.trim(F.col("item")))
+transactions_df = transactions_df.withColumn("coupon_code", F.trim(F.col("coupon_code")))
+transactions_df = transactions_df.fillna({"coupon_code":"NO_COUPON"})
+transactions_df = transactions_df.dropDuplicates()
+transactions_df = transactions_df.withColumn(
+        "total_amount",
+        F.col("quantity") * F.col("price")
+        )
+
+transactions_df = transactions_df.withColumn(
+        "purchase_date_ts",
+        F.totimestamp(F.col("purchase_date"))
+        )
